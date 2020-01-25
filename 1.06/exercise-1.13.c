76a65422c0b99c3ddde888fa532beef6fb91b9ea
Spencer Markin <markin.spencer@gmail.com>
2020-01-25 17:24:16 -0500

    exercise 1.13

---
+++ 1.06/exercise-1.13.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+#define MAXWORDLEN 15
+
+int main(void)
+{
+  int i, j;
+  int c, max, len, state;
+  int lengths[MAXWORDLEN];
+
+  for (i = 0; i < MAXWORDLEN; i++)
+  {
+    lengths[i] = 0;
+  }
+
+  state = OUT;
+  while ((c = getchar()) != EOF)
+  {
+    if (c == ' ' || c == '\n' || c == '\t')
+    {
+      if (state == IN)
+      {
+        state = OUT;
+        lengths[len - 1]++;
+      }
+    }
+    else
+    {
+      if (state == OUT)
+      {
+        state = IN;
+        len = 0;
+      }
+      len++;
+    }
+  }
+
+  max = 0;
+  for (i = 0; i < MAXWORDLEN; i++)
+  {
+    if (max < lengths[i])
+    {
+      max = lengths[i];
+    }
+  }
+
+  printf("\n");
+  for (i = max; i > 0; i--)
+  {
+    printf("%2d | ", i);
+    for (j = 0; j < MAXWORDLEN; j++)
+    {
+      if (lengths[j] >= i)
+      {
+        printf("#  ");
+      }
+      else
+      {
+        printf("%*c", 3, ' ');
+      }
+    }
+    printf("\n");
+  }
+
+  printf("%*c", 4, ' ');
+  for (i = 0; i < MAXWORDLEN; i++)
+  {
+    printf("---");
+  }
+  printf("\n");
+
+  printf("%*c", 4, ' ');
+  for (i = 0; i < MAXWORDLEN; i++)
+  {
+    printf("%2d ", i + 1);
+  }
+  printf("\n");
+}
