a05e0e2e9b6f6014157f41770fd10843c0a7dd9f
Spencer Markin <smarkin@questica.com>
2020-02-14 13:02:35 -0500

    exercise 1.18

---
+++ 1.09/exercise-1.18.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAX_LINE 1000
+
+int get_line(char[]);
+int trim(char []);
+
+int main(void) 
+{
+  int len;
+  char c;
+  char line[MAX_LINE];
+
+  while ((len = get_line(line)) != -1) {
+    if(len > 0) {
+      printf("string: %s, length: %d\n", line, len);
+      int trim_len = trim(line);    
+      printf("string: %s, length: %d\n", line, trim_len);
+    }   
+  }
+}
+
+int get_line(char line[]) 
+{
+  int i, c;
+  for (i=0; i < MAX_LINE && (c=getchar()) != EOF && c!='\n'; ++i) {
+    line[i] = c;
+  }
+
+  if(c == EOF) {
+    return -1;
+  }
+
+  line[i] = '\0';
+
+  return i;
+}
+
+int trim(char line[]) 
+{
+  int i, j;
+  char c;
+
+  i=0;
+  while ((c = line[i]) != '\0') {
+    if(c != ' ' && c != '\t') {
+      j = i;
+    }
+    ++i;
+  }
+    
+  line[++j] = '\0';
+
+  return j;
+}
